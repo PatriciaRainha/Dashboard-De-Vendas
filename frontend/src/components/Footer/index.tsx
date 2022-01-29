38bbc2ee7f389528492c81b2cff0935642944f65
PatriciaRainha <patricia.s.rainha@gmail.com>
2022-01-29 05:27:23 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/PatriciaRainha" target="_blank" rel="noreferrer">Patricia Rainha</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior !  <a href="https://www.linkedin.com/in/patricia-s-rainha/" target="_blank" rel="noreferrer">Patricia S.</a></small></p>
+            </div>
+        </footer>
+
+    );
+}
+
+export default Footer;
