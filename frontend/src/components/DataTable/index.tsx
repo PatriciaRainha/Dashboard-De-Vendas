b620f54ac3dfff2f654a9f93289ebaa003225dd6
PatriciaRainha <patricia.s.rainha@gmail.com>
2022-01-29 13:33:17 -0300

    UpdateData

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -13,93 +13,93 @@ function DataTable() {
                 </thead>
                 <tbody>
                     <tr>
-                        <td>22/04/2021</td>
+                        <td>22/04/2022</td>
                         <td>Patricia Rainha</td>
-                        <td>34</td>
+                        <td>24</td>
                         <td>25</td>
                         <td>15017.00</td>
                     </tr>
                     <tr>
-                        <td>22/04/2021</td>
+                        <td>22/04/2022</td>
                         <td>Patricia Rainha</td>
-                        <td>34</td>
+                        <td>24</td>
                         <td>25</td>
                         <td>15017.00</td>
                     </tr>
                     <tr>
-                        <td>22/04/2021</td>
+                        <td>22/04/2022</td>
                         <td>Patricia Rainha</td>
-                        <td>34</td>
+                        <td>24</td>
                         <td>25</td>
                         <td>15017.00</td>
                     </tr>
                     <tr>
-                        <td>22/04/2021</td>
+                        <td>22/04/2022</td>
                         <td>Patricia Rainha</td>
-                        <td>34</td>
+                        <td>24</td>
                         <td>25</td>
                         <td>15017.00</td>
                     </tr>
                     <tr>
-                        <td>22/04/2021</td>
+                        <td>22/04/2022</td>
                         <td>Patricia Rainha</td>
-                        <td>34</td>
+                        <td>24</td>
                         <td>25</td>
                         <td>15017.00</td>
                     </tr>
                     <tr>
-                        <td>22/04/2021</td>
+                        <td>22/04/2022</td>
                         <td>Patricia Rainha</td>
-                        <td>34</td>
+                        <td>24</td>
                         <td>25</td>
                         <td>15017.00</td>
                     </tr>
                     <tr>
-                        <td>22/04/2021</td>
+                        <td>22/04/2022</td>
                         <td>Patricia Rainha</td>
-                        <td>34</td>
+                        <td>24</td>
                         <td>25</td>
                         <td>15017.00</td>
                     </tr>
                     <tr>
-                        <td>22/04/2021</td>
+                        <td>22/04/2022</td>
                         <td>Patricia Rainha</td>
-                        <td>34</td>
+                        <td>24</td>
                         <td>25</td>
                         <td>15017.00</td>
                     </tr>
                     <tr>
-                        <td>22/04/2021</td>
+                        <td>22/04/2022</td>
                         <td>Patricia Rainha</td>
-                        <td>34</td>
+                        <td>24</td>
                         <td>25</td>
                         <td>15017.00</td>
                     </tr>
                     <tr>
-                        <td>22/04/2021</td>
+                        <td>22/04/2022</td>
                         <td>Patricia Rainha</td>
-                        <td>34</td>
+                        <td>24</td>
                         <td>25</td>
                         <td>15017.00</td>
                     </tr>
                     <tr>
-                        <td>22/04/2021</td>
+                        <td>22/04/2022</td>
                         <td>Patricia Rainha</td>
-                        <td>34</td>
+                        <td>24</td>
                         <td>25</td>
                         <td>15017.00</td>
                     </tr>
                     <tr>
-                        <td>22/04/2021</td>
+                        <td>22/04/2022</td>
                         <td>Patricia Rainha</td>
-                        <td>34</td>
+                        <td>24</td>
                         <td>25</td>
                         <td>15017.00</td>
                     </tr>
                     <tr>
-                        <td>22/04/2021</td>
+                        <td>22/04/2022</td>
                         <td>Patricia Rainha</td>
-                        <td>34</td>
+                        <td>24</td>
                         <td>25</td>
                         <td>15017.00</td>
                     </tr>
